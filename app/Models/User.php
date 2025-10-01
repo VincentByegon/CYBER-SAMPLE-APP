cbda68c6ff016a77b9ff52612b2c07167d31ba11
VincentByegon <kipkoechvincent11@gmail.com>
2025-10-01 18:05:41 +0300

    Added EnsureUserIsApproved middleware and forced HTTPS in production

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,9 +8,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 use Laravel\Fortify\TwoFactorAuthenticatable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable 
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, TwoFactorAuthenticatable;
