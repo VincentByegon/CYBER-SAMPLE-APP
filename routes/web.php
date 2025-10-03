1fadda7fca41062f2dfe5e7431efeda0ea83e256
VincentByegon <kipkoechvincent11@gmail.com>
2025-10-03 14:48:54 +0300

    web route 1

--- routes/web.php
+++ routes/web.php
@@ -93,10 +93,10 @@ Route::get('/invoices/monthly', function (\Illuminate\Http\Request $request) {
     // PDF route stays the same
    Route::get('/monthly-invoices/{id}/pdf', [\App\Http\Controllers\MonthlyInvoicePdfController::class, 'show'])->name('monthly-invoices.pdf'); 
 
-   Route::post('/validation', [MpesaCallbackController::class, 'validation']);
-Route::post('/confirmation', [MpesaCallbackController::class, 'confirmation']);
+
  Route::get('/mpesa/transactions', MpesaTransactions::class)
          ->name('mpesa.transactions');
 });
-
+   Route::post('/validation', [MpesaCallbackController::class, 'validation']);
+Route::post('/confirmation', [MpesaCallbackController::class, 'confirmation']);
 require __DIR__.'/auth.php';
